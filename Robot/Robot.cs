315c7276c69ffcdaa8fcd3b79c838c47d651b18a
Benjamin Eastmond <beastmond@ncm.com>
2018-03-29 15:21:51 -0600

    Add project files.

---
+++ Robot/Robot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using WPILib;
+using WPILib.Extras.AttributedCommandModel;
+
+namespace Robot
+{
+    public class Robot : AttributedRobot
+    {
+        // You don't need to add any code here unless AttributedRobot can't do something that you need to do.
+        // By default, you can leave this empty.
+    }
+}
